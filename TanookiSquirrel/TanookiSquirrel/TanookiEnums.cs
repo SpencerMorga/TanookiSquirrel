489575c2036549201acae0d7e426b6c680c68c5c
Spencer <spencer.morga@icloud.com>
2017-08-30 17:32:05 -0700

    Added 'Enums' class and put enums for tanooki, created 'TheGenuineTanooki'(the class for tanooki) and was halfway through it and did the git intro and learned how to do this

---
+++ TanookiSquirrel/TanookiSquirrel/TanookiEnums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TanookiSquirrel
+{
+    static class TanookiEnums
+    {
+        public enum TanookiFrames
+        {
+            Flying,
+            Tumbling,
+            Running,
+            Swimming
+
+        }
+
+
+
+    }
+}
---
+++ TanookiSquirrel/TanookiSquirrel/TheGenuineTanooki.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TanookiSquirrel
+{
+    class TheGenuineTanooki : MovingTanookiAnimation
+    {
+        Dictionary<TanookiEnums.TanookiFrames, List<Rectangle>> aneemayshun;
+        private TanookiEnums.TanookiFrames TanookiState;
+        TanookiEnums.TanookiFrames currentTanookiState
+        {
+            get
+            {
+                return TanookiState;
+            }
+            set
+            {
+                if (TanookiState != value)
+                {
+                    TanookiState = value;
+                    currentTanookiframeIndex = 0;
+                }
+            }
+        }
+
+
+
+        public TheGenuineTanooki(Texture2D image, Vector2 position, Vector2 speed, Color color, List<Rectangle> frames)
+            : base (image, position, speed, color, frames)
+        {
+            List<Rectangle> FlyingFrames = new List<Rectangle>()
+            {
+
+
+
+            };
+
+        }
+
+
+
+
+    }
+}
